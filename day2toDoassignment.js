0e5cf4256f7049d36b60ad7b80ba94c4bbfa88c7
Wesley Kolar <wrkolar@gmail.com>
2021-02-23 15:55:50 -0600

    does everything except remove from pending list

--- day2toDoassignment.js
+++ day2toDoassignment.js
@@ -5,17 +5,33 @@ btnSubmit.addEventListener("click",function() {
     let taskName = toDoTextBox.value 
     let liItem = document.createElement("li")
     liItem.innerHTML = taskName 
-    
+    let taskCheckBox = document.createElement("input")
+    taskCheckBox.setAttribute("type", "checkbox")
+    taskCheckBox.addEventListener("change", function(){
+        if (this.checked) {
+        console.log(this.parentElement) 
+        completedTasksUL.appendChild(this.parentElement)}
+        else {
+            taskEntryUL.appendChild(liItem)
+        }
+    }) 
     let removeButton = document.createElement("button")
     removeButton.innerHTML = "Remove"
+    
     removeButton.addEventListener("click", function() { 
-        console.log(this)
-        console.log(this.parentElement)
-        taskEntryUL.removeChild(this.parentElement)
+        if (this.parentElement == taskEntryUL){
+            console.log(this.parentElement)
+            taskEntryUL.removeChild(this.parentElement)}
+        else {
+            completedTasksUL.removeChild(this.parentElement)
+        }
+       
 
-    })
-    liItem.appendChild(removeButton)
+
+        })
+        
+    liItem.appendChild(taskCheckBox)
     taskEntryUL.appendChild(liItem)
+    liItem.appendChild(removeButton)
+   
     })
-
-
